c2848aef840ea45b9112ad3dc97e952fd24a34b8
redmango1446@gmail.com <redmango1446>
2023-07-24 22:26:12 +0900

    feat: MacBook타입 생성

---
+++ PopEasyTest/PopEasyTest/MacBook.swift
@@ -0,0 +1,13 @@
+//
+//  MacBook.swift
+//  PopEasyTest
+//
+//  Created by redmango1446 on 2023/07/24.
+//
+
+struct MacBook {
+    
+    mutating func chargerBattery(charger: Chargeabel) {
+        
+    }
+}
