13dbf4e77722927531d942a86e673c99122bd883
redmango1446@gmail.com <redmango1446>
2023-07-24 22:29:24 +0900

    feat: Charger타입 생성

---
+++ PopEasyTest/PopEasyTest/Charger.swift
@@ -0,0 +1,14 @@
+//
+//  Charger.swift
+//  PopEasyTest
+//
+//  Created by redmango1446 on 2023/07/24.
+//
+
+struct Charger: Chargeabel {
+    var maximumWattPerHour: WattPerHour
+    
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour {
+        <#code#>
+    }
+}
