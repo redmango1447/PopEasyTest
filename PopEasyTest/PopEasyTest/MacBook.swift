bce018c6277b7293d071e3f33f5a5a6c5ce4bd32
redmango1446@gmail.com <redmango1446>
2023-07-26 00:01:30 +0900

    test: 결과확인

--- PopEasyTest/PopEasyTest/MacBook.swift
+++ PopEasyTest/PopEasyTest/MacBook.swift
@@ -6,14 +6,14 @@
 //
 
 struct MacBook {
-    let 기기허용충전와트시: Watt = 20
-    let 현재저장된배터리용량 : Watt = 20
-    let 최대배터리용량: Watt = 100
+    private let 기기허용충전와트시: Watt = 20
+    private let 현재저장된배터리용량 : Watt = 20
+    private let 최대배터리용량: Watt = 100
 
     mutating func chargerBattery(charger: Chargeabel) {
         let 필요한배터리량 = 최대배터리용량 - 현재저장된배터리용량
         let 충전되는배터리량 = charger.convert(chargeableWattPerHour: 기기허용충전와트시)
         let 완충까지걸린시간 = 필요한배터리량/충전되는배터리량
-        print(완충까지걸린시간)
+        print("완충까지 걸린 시간: \(완충까지걸린시간)시간")
     }
 }
--- PopEasyTest/PopEasyTest/main.swift
+++ PopEasyTest/PopEasyTest/main.swift
@@ -13,3 +13,7 @@ let 아이폰고속충전기: Charger = Charger(maximumWattPerHour: 18)
 let 아이패드충전기: Charger = Charger(maximumWattPerHour: 30)
 let 맥북충전기: Charger = Charger(maximumWattPerHour: 96)
 let 고속맥북충전기: Charger = Charger(maximumWattPerHour: 106)
+
+var macBook = MacBook()
+
+macBook.chargerBattery(charger: 아이폰고속충전기)
